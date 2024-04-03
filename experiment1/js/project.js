717d5fe65ab3e3aac5e56e3ed6fa2c022ec94ac4
tblagarrett <tblagarrett@gmail.com>
2024-04-03 13:57:12 -0700

    Self-Help Book Title randomizer

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -1,6 +1,6 @@
 // project.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Garrett Blake
+// Date: 4/3/2024
 
 // NOTE: This is how we might start a basic JavaaScript OOP project
 
@@ -23,12 +23,44 @@ class MyProjectClass {
 }
 
 function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
+  // Format is verb, connector, noun
+  const fillers = {
+    begin: ["Conquer", "Dominate", "Live", "Master", "Feel", "", "Educate", "Stimulate"],
+    middle: ["your", "", ",", "more than just your"],
+    end: ["Emotions", "Habits", "Self", "Family", "Art", "Daily Routine", "Routine", "Bank Account", "Relationships", "Body"]
+  };
 
-  // call a method on the instance
-  myInstance.myMethod();
+  const template = `$begin $middle $end`;
+
+
+  // STUDENTS: You don't need to edit code below this line.
+
+  const slotPattern = /\$(\w+)/;
+
+  function replacer(match, name) {
+    let options = fillers[name];
+    if (options) {
+      return options[Math.floor(Math.random() * options.length)];
+    } else {
+      return `<UNKNOWN:${name}>`;
+    }
+  }
+
+  function generate() {
+    let story = template;
+    while (story.match(slotPattern)) {
+      story = story.replace(slotPattern, replacer);
+    }
+
+    /* global box */
+    $("#box").text(story);
+  }
+
+  /* global clicker */
+  $("#clicker").click(generate);
+
+  generate();
 }
 
 // let's get this party started - uncomment me
-//main();
+main();
